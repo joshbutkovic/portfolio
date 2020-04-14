f425568dbbad48ba7292b2a74265d75261e643d7
joshbutkovic <joshbutkovic@gmail.com>
2020-04-14 12:08:55 -0700

    small copy change

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ const Home: NextPage = () => {
                 Hi my name is Josh and I'm a passionate, experienced, and driven web developer. I have 7+ years of
                 professional experience. I have worked with a variety of languages and frameworks including React,
                 Django, and Express. I'm located in Henderson, NV just outside of Las Vegas. My day job is working for a
-                top US hotel company as a React/Java dev. I value well planned, clean, and scalable code written with
-                modern tooling. If my skillset fits your next project, get in touch for a free proposal.
+                top US hotel company as a React/Java developer. I value well planned, clean, and scalable code written
+                with modern tooling. If my skillset fits your next project, get in touch for a free proposal.
             </Mission>
             <Skills title="Skills" />
         </>
