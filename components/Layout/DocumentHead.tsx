ac59c24128e3577cd2f7051598ac7e51e763191e
joshbutkovic <joshbutkovic@gmail.com>
2020-04-21 20:49:53 -0700

    fixing ga script again

--- components/Layout/DocumentHead.tsx
+++ components/Layout/DocumentHead.tsx
@@ -19,17 +19,14 @@ const DocumentHead: React.FC<DocumentHeadProps> = ({ title }) => {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/site.webmanifest" />
-            <!-- Global site tag (gtag.js) - Google Analytics -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164271571-1"></script>
             <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag() {
+                window.dataLayer = window.dataLayer || []; function gtag(){' '}
+                {
                     // @ts-ignore
                     dataLayer.push(arguments)
                 }
-                gtag('js', new Date());
-
-                gtag('config', 'UA-164271571-1');
+                gtag('js', new Date()); gtag('config', 'UA-164271571-1');
             </script>
         </Head>
     );
